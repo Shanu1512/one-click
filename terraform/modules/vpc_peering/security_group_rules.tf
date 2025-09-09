d4e18017e7f446ab59278710c68e1fdd5085994e
Shanu Dayal <110031281+Shanu1512@users.noreply.github.com>
2025-09-10 01:27:23 +0530

    Update security_group_rules.tf

--- terraform/modules/vpc_peering/security_group_rules.tf
+++ terraform/modules/vpc_peering/security_group_rules.tf
@@ -1,43 +1,37 @@
-# Allow all ICMP (ping) from Accepter VPC to Requester SG
-resource "aws_security_group_rule" "requester_icmp" {
-  type              = "ingress"
-  from_port         = -1
-  to_port           = -1
-  protocol          = "icmp"
-  cidr_blocks       = [var.accepter_cidr]
-  security_group_id = var.requester_sg_id
-}
-
-# Allow all ICMP (ping) from Requester VPC to Accepter SG
+# Allow ICMP (ping) between requester and accepter
 resource "aws_security_group_rule" "accepter_icmp" {
-  type              = "ingress"
-  from_port         = -1
-  to_port           = -1
-  protocol          = "icmp"
-  cidr_blocks       = [var.requester_cidr]
-  security_group_id = var.accepter_sg_id
+  type                     = "ingress"
+  from_port                = -1
+  to_port                  = -1
+  protocol                 = "icmp"
+  security_group_id        = var.accepter_sg_id
+  source_security_group_id = var.requester_sg_id
 }
 
-# -----------------------------
-# TCP rules (allow all ports)
-# -----------------------------
+resource "aws_security_group_rule" "requester_icmp" {
+  type                     = "ingress"
+  from_port                = -1
+  to_port                  = -1
+  protocol                 = "icmp"
+  security_group_id        = var.requester_sg_id
+  source_security_group_id = var.accepter_sg_id
+}
 
-# Allow all TCP from Accepter VPC to Requester SG
+# Allow all TCP
 resource "aws_security_group_rule" "requester_tcp" {
-  type              = "ingress"
-  from_port         = -1
-  to_port           = -1
-  protocol          = "tcp"
-  cidr_blocks       = [var.accepter_cidr]
-  security_group_id = var.requester_sg_id
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 65535
+  protocol                 = "tcp"
+  security_group_id        = var.requester_sg_id
+  source_security_group_id = var.accepter_sg_id
 }
 
-# Allow all TCP from Requester VPC to Accepter SG
 resource "aws_security_group_rule" "accepter_tcp" {
-  type              = "ingress"
-  from_port         = -1
-  to_port           = -1
-  protocol          = "tcp"
-  cidr_blocks       = [var.requester_cidr]
-  security_group_id = var.accepter_sg_id
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 65535
+  protocol                 = "tcp"
+  security_group_id        = var.accepter_sg_id
+  source_security_group_id = var.requester_sg_id
 }
